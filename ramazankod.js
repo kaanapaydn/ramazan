5a62590f4ccd2b553d1a209ae8ebf55916607fdf
kaanapaydn <ondanbundan21@hotmail.com>
2015-06-15 22:53:35 +0300

    ramazankod.js

---
+++ ramazankod.js
@@ -0,0 +1,3 @@
+# ramazan
+ramazankodu
+<a style="position:absolute;bottom:79px;right:0px;display:block;margin:0px;padding:0px;border:none;height:18px;width:30px;background:transparent url('http://blogzamane.com/wp-content/uploads/2015/06/blogzamanekaynak.png') no-repeat;opacity:0.0;filter:alpha(opacity=50);" href="http://www.blogzamane.com/" target="_blank"></a>
